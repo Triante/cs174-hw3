5f6bdec54217dba7dc4626d5aa01998bf7ede8b4
penguinarmy94 <luisote94@gmail.com>
2017-04-22 23:01:30 -0700

    re-submit changes to index.php, not being pushed

--- index.php
+++ index.php
@@ -5,8 +5,18 @@
 */
 namespace jorgeandco\hw4;
 
-require_once('src//views//EditSheetView.php');
-use jorgeandco\hw4\views as CTV;
+require_once('vendor//autoload.php');
+use jorgeandco\hw4\controllers as CTV;
 
-$class = new CTV\EditSheetView();
-$class->render();
+if(isset($_REQUEST['arg1']))
+{
+	//$class = new CTV\Controller();
+	//$method = manage;
+}
+if (!isset($_REQUEST['c']) || !isset($_REQUEST['m']))
+{
+	$class = new CTV\MainController();
+	$method = view;
+	$class->$method(["page"=>"home"]);
+}
+
