e31eab39310fa4d303cf10432b14afae1d513029
Jorge Aguiniga <jorge.aguiniga94@gmail.com>
2017-04-26 21:25:28 -0700

    footer and header doc

--- src/views/layouts/Footer.php
+++ src/views/layouts/Footer.php
@@ -5,13 +5,12 @@
 namespace jorgeandco\hw4\views\layouts;
 
 /**
-* Layout class for writing the closing tags for a validating XHTML5 document
+* Footer class for writing the closing tags for a validating XHTML5 document
 */
 class Footer
 {
     /**
     *   Renders the footer of the application by writing the closing tags of the application's XHTML5 document
-    *   @param String $data (not used)
     */
     function render()
     {
